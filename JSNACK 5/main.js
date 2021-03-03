e34beb3ee61339c92d2295e7bbfaff02af643ea1
Claudio <claudiocarchietti@yahoo.it>
2021-03-03 15:37:10 +0100

    Esercizio 6 terminato

--- JSNACK 5/main.js
+++ JSNACK 5/main.js
@@ -3,8 +3,8 @@ listanumeridispari= [];
 for (var i = 0; i < 6; i++) {
   var numeri = parseInt(prompt("Dammi un numero"))
   if (numeri %2){
-    var listanumeri = [numeri];
-    listanumeridispari.push(listanumeri);
+    // var listanumeri = [numeri];
+    listanumeridispari.push(numeri);
   }
 }
 
---
+++ JSNACK 6/main.js
@@ -0,0 +1,5 @@
+var numero = prompt("Dammi un numero.")
+console.log(numero);
+
+var cubonumero = numero * numero * numero;
+console.log(cubonumero);
