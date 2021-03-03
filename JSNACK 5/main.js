8829e624c5c0cc5e9103fef1d1e9832cb0a0a839
Claudio <claudiocarchietti@yahoo.it>
2021-03-03 15:25:42 +0100

    esercizio 5 terminato

---
+++ JSNACK 5/main.js
@@ -0,0 +1,13 @@
+listanumeridispari= [];
+
+for (var i = 0; i < 6; i++) {
+  var numeri = parseInt(prompt("Dammi un numero"))
+  if (numeri %2){
+    var listanumeri = [numeri];
+    listanumeridispari.push(listanumeri);
+  }
+}
+
+console.log(listanumeridispari);
+// var listanumerifinale = [listanumeri];
+// console.log(listanumerifinale);
